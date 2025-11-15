4ec8784550239e61251e32f5aae6f11a4202b287
Sava <sava41@gmail.com>
2025-11-15 13:19:14 -0500

    dominant light direction implementation

--- intern/cycles/scene/pass.cpp
+++ intern/cycles/scene/pass.cpp
@@ -102,6 +102,7 @@ const NodeEnum *Pass::get_type_enum()
     pass_type_enum.insert("bake_primitive", PASS_BAKE_PRIMITIVE);
     pass_type_enum.insert("bake_seed", PASS_BAKE_SEED);
     pass_type_enum.insert("bake_differential", PASS_BAKE_DIFFERENTIAL);
+    pass_type_enum.insert("bake_dominant_direction", PASS_BAKE_DOMINANT_DIRECTION);
 
 #ifdef WITH_CYCLES_DEBUG
     pass_type_enum.insert("guiding_color", PASS_GUIDING_COLOR);
@@ -374,6 +375,11 @@ PassInfo Pass::get_info(const PassType type,
       pass_info.num_components = 4;
       pass_info.use_exposure = false;
       pass_info.use_filter = false;
+      break;
+    case PASS_BAKE_DOMINANT_DIRECTION:
+      pass_info.num_components = 3;
+      pass_info.use_exposure = false;
+      pass_info.use_filter = true;
       break;
 
     case PASS_CATEGORY_LIGHT_END:
